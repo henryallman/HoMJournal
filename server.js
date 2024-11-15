edf4a480e738791f29741c8f01f6a88ada29c8c8
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2024-11-15 14:16:36 -0600

    Revert "Created WebSocket Module"
    This reverts commit 72b16546ba51d0e6371c8630783c6e587af6b282.

--- server.js
+++ server.js
@@ -2,22 +2,3 @@
  * main Javascript file for the application
  *  this file is executed by the Node server
  */
-
-// Import the http module, which provides an HTTP server
-const http = require("http");
-
-// Import the express module, which provides the express function
-const express = require("express");
-
-// Create an Express application with an HTTP server
-const app = express();
-const server = http.createServer(app);
-
-// Create a new WebSocket server object using our created function
-const {createSocketServer} = require("./server/socket/socket");
-createSocketServer(server);
-
-// Start the server on port 8080
-server.listen(8080, () => {
-    console.log("The server is listening on localhost:8080");
-})
