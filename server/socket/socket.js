72b16546ba51d0e6371c8630783c6e587af6b282
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2024-11-15 14:14:17 -0600

    Created WebSocket Module
    Started server and created WebSocket module

---
+++ server/socket/socket.js
@@ -0,0 +1,28 @@
+// Create a WebSocket
+
+// import the Server functionality from socket.io
+const {Server} = require("socket.io")
+
+// Variable to store the WebSocket server
+let io;
+
+function createSocketServer(httpServer) {
+    io = new Server(httpServer, {
+        connectionStateRecovery: {},
+    });
+
+    // Turn on the WebSocket
+    io.on("connection", (socket) => {
+        console.log("A user is connected.");
+
+        // Specify what happens when a user disconnects
+        socket.on("disconnect", () => {
+            console.log("A user disconnected.");
+        });
+    });
+
+    return io;
+}
+
+// Export any functions so they can be used outside this file
+module.exports = {createSocketServer};
